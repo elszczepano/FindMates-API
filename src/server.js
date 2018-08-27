6854d26e92c4fd6903acee1a8ad4501d6cfee20d
Dominik <szczepaniak.dominik98@gmail.com>
2018-08-27 20:23:16 +0200

    Setup basic Express.js server

---
+++ src/server.js
@@ -0,0 +1,7 @@
+import app from './app';
+
+app.set('port', process.env.PORT || 8080);
+
+const server = app.listen(app.get('port'), () => {
+    console.log(`Listening on ${server.address().port}`);
+});
