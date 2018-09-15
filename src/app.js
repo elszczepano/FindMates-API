78213431ac1b8a122cbeb1af472cb971a11d9c53
Dominik <szczepaniak.dominik98@gmail.com>
2018-09-15 19:25:51 +0200

    Setup routing | Setup mongoose

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import usersRouter from './routes/usersRouter';
+import matchesRouter from './routes/matchesRouter';
+import messagesRouter from './routes/messagesRouter';
 
+//Setup Express.js
 const app = express();
+
+//Setup MongoDB
+mongoose.connect('mongodb://localhost:27017/FindMates', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+//Setup routing
+app.use('/matches', matchesRouter);
+app.use('/messages', messagesRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
