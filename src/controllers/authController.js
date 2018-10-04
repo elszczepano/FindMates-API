bd41937a9ce7abdc8d89f9fe5617e36f771d4cae
Dominik <szczepaniak.dominik98@gmail.com>
2018-10-04 20:35:54 +0200

    Handle passing JWT Token in response when sign in successfull

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,14 @@
 import User from '../models/User';
+import jwt from 'jsonwebtoken';
 
 export default {
     async login(req, res, next) {
+
+        const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, {expiresIn: 3600});
+
         return res.status(200).json({
-            message: "User signed in succesfully"
+            message: "User signed in succesfully",
+            token: token
         });
     },
 
