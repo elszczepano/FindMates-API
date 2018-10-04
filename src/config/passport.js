4135e37b129e677070f205b4147fa2714f65f6ff
Dominik <szczepaniak.dominik98@gmail.com>
2018-10-04 20:46:39 +0200

    Add JWT strategy

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,6 +1,22 @@
 import passport from 'passport';
+import passportJWT from 'passport-jwt';
 import User from '../models/User';
 
-export default () => {
-    passport.use(User.createStrategy())
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+function verifyCallback(payload, done) {
+    return User.findOne({_id: payload.id})
+        .then(user => done(null, user))
+        .catch(err => done(err));
 }
+
+export default () => {
+    const config = {
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        secretOrKey: process.env.JWT_SECRET
+    };
+    passport.use(User.createStrategy());
+    passport.use(new JWTStrategy(config, verifyCallback));
+}
+
