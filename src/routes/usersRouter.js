9dfb8ad83e62ffd2171636d5ae6a62bf8e3b8583
Dominik <szczepaniak.dominik98@gmail.com>
2018-10-04 20:47:42 +0200

    Use autenticate middleware

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,21 +1,27 @@
 import { Router } from 'express';
+import jwtAuth from '../middlewares/auth';
 import usersController from "../controllers/usersController";
 
 const api = Router();
 
 api.get('/',
+    jwtAuth,
     usersController.getAll
 );
 api.get('/:id',
+    jwtAuth,
     usersController.getOne
 );
 api.post('/',
+    jwtAuth,
     usersController.createNew
 );
 api.put('/:id',
+    jwtAuth,
     usersController.updateOne
 );
 api.delete('/:id',
+    jwtAuth,
     usersController.deleteOne
 );
 
