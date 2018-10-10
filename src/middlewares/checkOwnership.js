73d3777a9f1c75afb47ce8ecff06fe49a7f8c3b7
Dominik <szczepaniak.dominik98@gmail.com>
2018-10-10 22:07:54 +0200

    Add middleware to check ownership of resource

---
+++ src/middlewares/checkOwnership.js
@@ -0,0 +1,9 @@
+export default (req, res, next) => {
+    const userId = req.user._id;
+    const queryId = req.params.id;
+    if(queryId == userId) next();
+    else res.status(403).json({
+        success: false,
+        message: 'Access denied. User not permitted'
+    })
+}
