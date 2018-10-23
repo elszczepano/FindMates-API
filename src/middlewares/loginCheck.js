b20a24b4c7e30711b8d21fcb8ee79daaa881dce5
Dominik <szczepaniak.dominik98@gmail.com>
2018-10-23 20:14:52 +0200

    Separate login middleware to the new file | Create custom error responses in JSON format.

--- src/middlewares/loginCheck.js
+++ src/middlewares/loginCheck.js
@@ -1,14 +1,14 @@
 import passport from 'passport';
 
 export default (req, res, next) => {
-    return passport.authenticate('jwt',{session: false}, (err, user, info) => {
+    return passport.authenticate('local',{session: false}, (err, user, info) => {
         if (err) return next(err);
         if (!user) {
             return res.status(401).json({
                 success: false,
-                error: info.message || 'Invalid token.'
-                }
-            )}
+                error: info.message || 'An error occurred.'
+            })
+        }
         next();
     })(req, res, next);
 }
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import authController from "../controllers/authController";
-import passport from 'passport';
+import loginCheck from "../middlewares/loginCheck";
 
 const api = Router();
 
-api.post('/login', passport.authenticate('local', { session: false }), authController.login);
+api.post('/login', loginCheck, authController.login);
 api.post('/register', authController.register);
 
 module.exports = api;
