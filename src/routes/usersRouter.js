1f367181003687868e7fd98bf96a3f5079101eae
Dominik <szczepaniak.dominik98@gmail.com>
2018-11-08 18:17:56 +0100

    Create user update validation.

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -3,6 +3,8 @@ import jwtAuth from '../middlewares/auth';
 import checkOwnership from '../middlewares/checkOwnership';
 import checkPrivileges from "../middlewares/checkPrivileges";
 import usersController from "../controllers/usersController";
+import { validateUserUpdate } from "../validators/usersValidator";
+import { checkValidation } from "../validators/checkValidation";
 
 const api = Router();
 api.get('/',
@@ -17,6 +19,8 @@ api.get('/:id',
 api.put('/:id',
     jwtAuth,
     checkOwnership,
+    validateUserUpdate,
+    checkValidation,
     usersController.updateOne
 );
 api.delete('/:id',
--- src/validators/usersValidator.js
+++ src/validators/usersValidator.js
@@ -16,3 +16,17 @@ exports.validateRegister = [
     sanitizeBody('*').escape()
 ];
 
+exports.validateUserUpdate = [
+    check('name').optional().trim().not().isEmpty().withMessage('Name is required.'),
+    check('password').optional().trim().not().isEmpty().withMessage('Password is required.'),
+    check('password').trim().isLength({ min: 8 }).withMessage('Password must be at least 8 characters long.'),
+    check('email').optional().trim().not().isEmpty().withMessage('E-mail is required.'),
+    check('email').trim().isEmail().withMessage('Invalid e-mail address.'),
+    check('phone').optional().trim().not().isEmpty().withMessage('Phone number is required.'),
+    check('gender').optional().trim().not().isEmpty().withMessage('Gender is required.'),
+    check('birthDate').optional().trim().not().isEmpty().withMessage('Date of birth is required.'),
+    check('birthDate').trim().matches(/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/).withMessage('Invalid date of birth.'),
+    check('pictures').optional().isArray().withMessage('Invalid data format.'),
+    check('purpose').optional().trim().not().isEmpty().withMessage('Purpose is required.'),
+    sanitizeBody('*').escape()
+];
