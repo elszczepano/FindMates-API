e920667b86901372dbcf3a420f7ac511807fecc5
Dominik <szczepaniak.dominik98@gmail.com>
2018-12-21 11:38:25 +0100

    Use destructuring to get properties of an object.

--- src/middlewares/checkOwnership.js
+++ src/middlewares/checkOwnership.js
@@ -1,7 +1,7 @@
 export default (req, res, next) => {
-    const userId = req.user._id;
-    const queryId = req.params.id;
-    if(queryId == userId) next();
+    const { _id } = req.user;
+    const { id } = req.params;
+    if(_id == id) next();
     else res.status(403).json({
         success: false,
         message: 'Access denied. User not permitted.'
