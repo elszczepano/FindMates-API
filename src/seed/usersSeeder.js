e9c399e4fdffe322fed01d0737d02f2ae9fb2121
elszczepano <szczepaniak.dominik98@gmail.com>
2018-12-22 15:41:10 +0100

    Rewrite usersSeeder - now user has a paassword.

--- src/seed/usersSeeder.js
+++ src/seed/usersSeeder.js
@@ -1,4 +1,4 @@
-import faker from 'faker';
+import API from '../../testsConfig';
 import mongoose from 'mongoose';
 import User from '../models/User';
 
@@ -6,38 +6,20 @@ mongoose.connect('mongodb://localhost:27017/FindMates', { useNewUrlParser: true 
 mongoose.set('useCreateIndex', true);
 User.deleteMany({}, err=> console.log(err));
 
-const user = new User({
-    name: faker.name.firstName(),
-    email: faker.internet.email(),
-    phone: faker.phone.phoneNumber(),
-    gender: 'male',
-    birthDate: faker.date.past(),
-    geometry: {
-        coordinates: [52.406374, 16.9251681]
-    },
-    pictures: [
-        faker.image.image(),
-        faker.image.image(),
-        faker.image.image()
-    ],
-    profilePicture: faker.image.image(),
-    description: faker.lorem.sentence(),
-    snapchat: faker.lorem.word(),
-    instagram: faker.lorem.word(),
-    purpose: 'Friends',
-});
 
-const register = new Promise((resolve, reject) => {
-    User.register(user, 'test', (err, account) => {
-        if(err) {
-            reject(err);
-        }
-        resolve('User seed successfully.');
-    });
-});
-register.then(res => {
-    console.log(res);
-    mongoose.disconnect();
-}).catch(err => console.log(err));
+API.post('register', {
+    name: "John",
+    phone: "123 456 789",
+    password: "secret123",
+    email: "john@doe.com",
+    gender: "male",
+    birthDate: "1992-12-04",
+    purpose: "Friends"
+    }).then(item => {
+        console.log('User seed successfully.');
+        mongoose.disconnect();
+    })
+    .catch(err => console.log(err));
+
 
 
