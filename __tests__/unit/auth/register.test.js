4aec57764e18b23f443cbc84bbfa7dee11bdb7a8
elszczepano <szczepaniak.dominik98@gmail.com>
2018-12-23 15:15:56 +0100

    Replace library for mocking request and response.

--- __tests__/unit/auth/register.test.js
+++ __tests__/unit/auth/register.test.js
@@ -1,20 +1,20 @@
 import authController from '../../../src/controllers/authController';
-import { mockRequest, mockResponse } from 'mock-req-res';
+import httpMocks from "node-mocks-http";
 
 
 test('Check if user signed up correctly', () => {
-    const req = mockRequest();
-    const res = mockResponse();
-
-    req.body = {
-        name: "Jane",
-        phone: "123 456 789",
-        password: "secret123",
-        email: "jane@doe.com",
-        gender: "female",
-        birthDate: "1992-12-04",
-        purpose: "Friends"
-    };
+    const req = httpMocks.createRequest({
+        body: {
+            name: "Jane",
+            phone: "123 456 789",
+            password: "secret123",
+            email: "jane@doe.com",
+            gender: "female",
+            birthDate: "1992-12-04",
+            purpose: "Friends"
+        }
+    });
+    const res = httpMocks.createResponse();
     authController.register(req, res).then(item => expect(item.data).toEqual({
         success: true,
         message: "User registered successfully."
