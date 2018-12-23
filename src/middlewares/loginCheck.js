9600a2d875a6e4c1da3fe43161ef78d1c6de995d
elszczepano <szczepaniak.dominik98@gmail.com>
2018-12-23 15:38:28 +0100

    Remove unnecessary return.

--- src/middlewares/loginCheck.js
+++ src/middlewares/loginCheck.js
@@ -1,13 +1,13 @@
 import passport from 'passport';
 
 export default (req, res, next) => {
-    return passport.authenticate('local',{session: false}, (err, user, info) => {
+     passport.authenticate('local',{session: false}, (err, user, info) => {
         if (err) return next(err);
         if (!user) {
             return res.status(401).json({
                 success: false,
                 error: info.message || 'An error occurred.'
-            })
+            });
         }
         req.user = user;
         next();
