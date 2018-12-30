d68b39fb7d8176726f5dd2b1ca7a87159ec1be79
Dominik <szczepaniak.dominik98@gmail.com>
2018-12-30 19:58:24 +0100

    Checking ID in a proper method.

--- src/middlewares/checkOwnership.js
+++ src/middlewares/checkOwnership.js
@@ -1,7 +1,5 @@
 export default (req, res, next) => {
-    const { _id } = req.user;
-    const { id } = req.params;
-    if(_id == id) next();
+    if(req.user._id.equals(req.params.id)) next();
     else res.status(403).json({
         success: false,
         message: 'Access denied. User not permitted.'
