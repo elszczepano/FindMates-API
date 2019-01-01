4eb9686eac1e11847e030287efbfd9c55d2d672e
Dominik <szczepaniak.dominik98@gmail.com>
2019-01-01 19:31:01 +0100

    Import assert module.

--- tests/auth/authController.test.js
+++ tests/auth/authController.test.js
@@ -1,10 +1,11 @@
 import authController from '../../src/controllers/authController';
+import assert from 'assert';
 
 describe('Test whether register and login are functions', () => {
     it('Login function should be defined', () => {
-        console.assert(typeof authController.login === 'function');
+        assert(typeof authController.login === 'function');
     });
     it('Register function should be defined', () => {
-        console.assert(typeof authController.register === 'function');
+        assert(typeof authController.register === 'function');
     });
 });
