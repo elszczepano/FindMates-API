66c88bb7e3a503beedb06b8ee24d3f067be64a49
Dominik <szczepaniak.dominik98@gmail.com>
2019-01-17 22:58:43 +0100

    Remove useless test | Prepare further test case.

--- tests/unit/auth/authController.test.js
+++ tests/unit/auth/authController.test.js
@@ -1,16 +1,6 @@
 import authController from '../../../src/controllers/authController';
-import assert from 'assert';
 import { expect } from 'chai';
 import sinon from 'sinon';
-
-describe('Test whether register and login are methods', () => {
-    it('Login method should be defined', () => {
-        assert(typeof authController.login === 'function');
-    });
-    it('Register method should be defined', () => {
-        assert(typeof authController.register === 'function');
-    });
-});
 
 describe('Test login method', () => {
     it('Login method should return JWT', done => {
@@ -27,11 +17,18 @@ describe('Test login method', () => {
             },
             json: sinon.spy()
         };
-        
         authController.login(req,res).then(() => {
             expect(res.json.firstCall.lastArg.token).to.be.a('string');
             done();
         });
     });
+});
 
+describe('Test register method', () => {
+    it('Register method should create new user', done => {
+
+    });
+    it('Register method should emit an error', done => {
+
+    });
 });
