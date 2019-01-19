0ef3667eb8eab0a1ceed87248207aaba6ec277df
Dominik <szczepaniak.dominik98@gmail.com>
2019-01-19 21:40:18 +0100

    Test getAll method.

--- tests/unit/controllers/errorTicketsController.test.js
+++ tests/unit/controllers/errorTicketsController.test.js
@@ -1,20 +1,27 @@
 import errorTicketsController from '../../../src/controllers/errorTicketsController';
-import assert from 'assert';
+import mongoose from "mongoose";
+import sinon from "sinon";
+import {expect} from "chai";
 
-describe('Test if error ticket methods exists', () => {
-    it('getAll method should be defined', () => {
-        assert(typeof errorTicketsController.getAll === 'function');
-    });
-    it('getOne method should be defined', () => {
-        assert(typeof errorTicketsController.getOne === 'function');
-    });
-    it('getResourcesOfUser method should be defined', () => {
-        assert(typeof errorTicketsController.getResourcesOfUser === 'function');
-    });
-    it('createNew method should be defined', () => {
-        assert(typeof errorTicketsController.createNew === 'function');
-    });
-    it('deleteOne method should be defined', () => {
-        assert(typeof errorTicketsController.deleteOne === 'function');
+
+
+describe('Test method getAll', () => {
+    it('getAll should return all records', (done) => {
+        const req = {
+            query: {
+                offset: 0,
+                perPage: 10
+            }
+        };
+        const res = {
+            status: function () {
+                return this;
+            },
+            json: sinon.spy()
+        };
+        errorTicketsController.getAll(req,res).then(() => {
+            expect(res.json.firstCall.lastArg.success).to.equal(true);
+            done();
+        });
     });
 });
