81711fdae867a1736570b61c6e18329dd19f01a6
Dominik <szczepaniak.dominik98@gmail.com>
2019-01-20 17:03:56 +0100

    Add test 404 error to getAll method.

--- tests/unit/controllers/errorTicketsController.test.js
+++ tests/unit/controllers/errorTicketsController.test.js
@@ -3,7 +3,7 @@ import sinon from "sinon";
 import {expect} from "chai";
 
 describe('Test method getAll', () => {
-    it('getAll should return all records', (done) => {
+    it('getAll should return an error', done => {
         const req = {
             query: {
                 offset: 0,
@@ -16,9 +16,36 @@ describe('Test method getAll', () => {
             },
             json: sinon.spy()
         };
-        errorTicketsController.getAll(req,res).then(() => {
-            expect(res.json.firstCall.lastArg.success).to.equal(true);
-            done();
+        errorTicketsController.createNew(req,res).then(() => {
+            errorTicketsController.getAll(req,res).then(() => {
+                expect(res.json.firstCall.lastArg.success).to.equal(false);
+                done();
+            });
+        });
+    });
+    it('getAll should return all records', done => {
+        const req = {
+            body: {
+                user: '5c308c78ce1c640bdc493942',
+                message: 'lorem ipsum'
+            },
+            query: {
+                offset: 0,
+                perPage: 10
+            }
+        };
+        const res = {
+            status: function () {
+                return this;
+            },
+            json: sinon.spy()
+        };
+        errorTicketsController.createNew(req,res).then(() => {
+            errorTicketsController.getAll(req,res).then(() => {
+                expect(res.json.firstCall.lastArg.success).to.equal(true);
+                done();
+            });
         });
     });
 });
+
