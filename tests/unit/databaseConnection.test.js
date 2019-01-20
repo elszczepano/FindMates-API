21c4cc5b843b356e93f77f3d2df77c0dbcda9b6f
Dominik <szczepaniak.dominik98@gmail.com>
2019-01-20 17:01:12 +0100

    Clear database before tests.

--- tests/unit/databaseConnection.test.js
+++ tests/unit/databaseConnection.test.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config({path: '.env'});
 
 before(done => {
-    mongoose.connect(process.env.DB_HOST, {useNewUrlParser: true });
+    mongoose.connect(process.env.DB_HOST, {useNewUrlParser: true },() => {
+        mongoose.connection.db.dropDatabase();
+    });
     mongoose.set('useCreateIndex', true);
     mongoose.set('useFindAndModify', false);
     done();
