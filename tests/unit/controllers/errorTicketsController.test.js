d44c837ba7afa26b9a86a2bf81c793a76d930e16
Dominik <szczepaniak.dominik98@gmail.com>
2019-01-21 17:23:25 +0100

    Create test for getResourcesOfUser and create 500 error tests.

--- tests/unit/controllers/errorTicketsController.test.js
+++ tests/unit/controllers/errorTicketsController.test.js
@@ -4,8 +4,8 @@ import {expect} from "chai";
 
 let idStore= '';
 
-describe('Test method getAll', () => {
-    it('getAll should return an error', done => {
+describe('Test errorTicketsController getAll method', () => {
+    it('getAll should return an 404 error', done => {
         const req = {
             query: {
                 offset: 0,
@@ -49,7 +49,7 @@ describe('Test method getAll', () => {
     });
 });
 
-describe('Test method createNew', () => {
+describe('Test errorTicketsController createNew method', () => {
     it('createNew should return new record', done => {
         const req = {
             body: {
@@ -69,13 +69,70 @@ describe('Test method createNew', () => {
             done();
         });
     });
+    it('createNew should return an 500 error', done => {
+        const req = {
+            body: {
+                user: '',
+                message: ''
+            }
+        };
+        const res = {
+            status: function () {
+                return this;
+            },
+            json: sinon.spy()
+        };
+        errorTicketsController.createNew(req,res).then(() => {
+            throw "Emit an error!";
+        }).catch(() => {
+            expect(res.json.firstCall.lastArg.success).to.equal(false);
+            done();
+        });
+    });
 });
 
-describe('Test method getOne', () => {
-    it('getOne should return an error', done => {
+describe('Test errorTicketsController getResourcesOfUser method',() => {
+    it('getResourcesOfUser should return an 404 error', done => {
+        const req = {
+            params: {
+                id: ''
+            }
+        };
+        const res = {
+            status: function () {
+                return this;
+            },
+            json: sinon.spy()
+        };
+        errorTicketsController.getResourcesOfUser(req,res).then(() => {
+            expect(res.json.firstCall.lastArg.success).to.equal(false);
+            done();
+        });
+    });
+    it('getResourcesOfUser should return records of exact user', done => {
         const req = {
             params: {
                 id: '5c308c78ce1c640bdc493942'
+            }
+        };
+        const res = {
+            status: function () {
+                return this;
+            },
+            json: sinon.spy()
+        };
+        errorTicketsController.getResourcesOfUser(req,res).then(() => {
+            expect(res.json.firstCall.lastArg.success).to.equal(true);
+            done();
+        });
+    });
+});
+
+describe('Test errorTicketsController getOne method', () => {
+    it('getOne should return an 404 error', done => {
+        const req = {
+            params: {
+                id: ''
             }
         };
         const res = {
@@ -108,8 +165,8 @@ describe('Test method getOne', () => {
     });
 });
 
-describe('Test method deleteOne', () => {
-    it('deleteOne should return an error', done => {
+describe('Test errorTicketsController deleteOne method', () => {
+    it('deleteOne should return an 404 error', done => {
         const req = {
             params: {
                 id: '5c308c78ce1c640bdc493942'
@@ -122,6 +179,25 @@ describe('Test method deleteOne', () => {
             json: sinon.spy()
         };
         errorTicketsController.deleteOne(req,res).then(() => {
+            expect(res.json.firstCall.lastArg.success).to.equal(false);
+            done();
+        });
+    });
+    it('deleteOne should return an 500 error', done => {
+        const req = {
+            params: {
+                id: "1"
+            }
+        };
+        const res = {
+            status: function () {
+                return this;
+            },
+            json: sinon.spy()
+        };
+        errorTicketsController.deleteOne(req,res).then(() => {
+            throw "Emit an error";
+        }).catch(() => {
             expect(res.json.firstCall.lastArg.success).to.equal(false);
             done();
         });
