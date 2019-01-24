63cd37996e7b3641c287eb66350d42d2a6ad1b34
Dominik <szczepaniak.dominik98@gmail.com>
2019-01-24 18:42:27 +0100

    Fix bugs. | Prettify code.

--- src/controllers/messagesController.js
+++ src/controllers/messagesController.js
@@ -1,213 +1,209 @@
 import Message from '../models/Message';
 import Match from '../models/Match';
 
-exports.getAll = (req, res) => {
-    const offset = parseInt(req.query.offset) || 0;
-    const perPage = parseInt(req.query.perPage) || 100;
-    return Message.find({})
-        .populate({
-            path: 'recipient',
-            select: 'name profilePicture -_id'
-        })
-        .populate({
-            path: 'sender',
-            select: 'name profilePicture -_id'
-        })
-        .populate({
-            path: 'match'
-        })
-        .skip(offset).limit(perPage)
-        .then(item => {
-            if(!item.length) return res.status(404).json({
-                success: false,
-                message: "Messages not found."
-            });
-            return res.status(200).json({
-                success: true,
-                data: item
-            });
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.getOne = (req, res) => {
-    return Message.findById(req.params.id)
-        .populate({
-            path: 'recipient',
-            select: 'name profilePicture -_id'
-        })
-        .populate({
-            path: 'sender',
-            select: 'name profilePicture -_id'
-        })
-        .populate({
-            path: 'match'
-        })
-        .then(item => {
-            if(!item.length) return res.status(404).json({
-                success: false,
-                message: `Message with ID ${req.params.id} not found.`
-            });
-            if(req.user._id.equals(item.recipient._id) || req.user._id.equals(item.sender._id)) {
-                res.status(200).json({
+export default {
+    getAll(req, res) {
+        const offset = parseInt(req.query.offset) || 0;
+        const perPage = parseInt(req.query.perPage) || 100;
+        return Message.find({})
+            .populate({
+                path: 'recipient',
+                select: 'name profilePicture -_id'
+            })
+            .populate({
+                path: 'sender',
+                select: 'name profilePicture -_id'
+            })
+            .populate({
+                path: 'match'
+            })
+            .skip(offset).limit(perPage)
+            .then(item => {
+                if(!item.length) return res.status(404).json({
+                    success: false,
+                    message: "Messages not found."
+                });
+                return res.status(200).json({
                     success: true,
                     data: item
                 });
-            } else {
-                res.status(403).json({
-                    success: false,
-                    message: 'Access denied. User not permitted.'
-                });
-            }
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.getResourcesOfUser = (req, res) => {
-    return Message.find({$or:
-            [
-                {'recipient': req.user._id},
-                {'sender': req.user._id}
-            ]})
-        .then(item => {
-            if(!item.length) return res.status(404).json({
+            })
+            .catch(err => res.status(500).json({
                 success: false,
-                message: `Resources of user ID ${req.params.id} not found.`
-            });
-            if(req.user._id.equals(item.recipient._id) || req.user._id.equals(item.sender_id)) {
-                res.status(200).json({
-                    success: true,
-                    data: item
-                });
-            } else {
-                res.status(403).json({
+                message: err
+            }));
+    },
+    getOne(req, res) {
+        return Message.findById(req.params.id)
+            .populate({
+                path: 'recipient',
+                select: 'name profilePicture -_id'
+            })
+            .populate({
+                path: 'sender',
+                select: 'name profilePicture -_id'
+            })
+            .populate({
+                path: 'match'
+            })
+            .then(item => {
+                if(!item.length) return res.status(404).json({
                     success: false,
-                    message: 'Access denied. User not permitted.'
+                    message: `Message with ID ${req.params.id} not found.`
                 });
-            }
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.getResourcesOfMatch = (req, res) => {
-    return Message.find({'match': req.params.id})
-        .then(item => {
-            if(!item.length) return res.status(404).json({
-                success: false,
-                message: `Resources of match ID ${req.params.id} not found.`
-            });
-            if(req.user._id.equals(item.recipient._id) || req.user._id.equals(item.sender._id)) {
-                res.status(200).json({
-                    success: true,
-                    data: item
-                });
-            } else {
-                res.status(403).json({
-                    success: false,
-                    message: 'Access denied. User not permitted.'
-                });
-            }
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.createNew = (req, res) => {
-    Match.findById(req.body.match)
-        .then(item => {
-            if(!item.length) {
-                return res.status(404).json({
-                    success: false,
-                    message: `Match with ID ${req.body.match} not found.`
-                });
-            }
-            if(!req.user._id.equals(item.user1._id) || !req.user._id.equals(item.user2._id)) {
-                return res.status(403).json({
-                    success: false,
-                    message: 'Invalid match. User not permitted.'
-                });
-            }
-        });
-    if(req.user._id.equals(req.body.recipient) || !req.user._id.equals(req.body.sender)) {
-        return res.status(403).json({
-            success: false,
-            message: 'Access denied. User not permitted.'
-        });
-    }
-    const message = new Message(req.body);
-    return message.save()
-        .then(item => res.status(201).json({
-            success: true,
-            message: "Message created successfully.",
-            data: item
-        }))
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.updateOne = (req, res) => {
-    return Message.findById(req.params.id)
-        .then(item => {
-            if(!item.length) return res.status(404).json({
-                success: false,
-                message: `Message with ID ${req.params.id} not found.`
-            });
-            if(req.user._id.equals(item.sender._id)) {
-                const message = Object.assign(item, req.body);
-                message.save()
-                    .then(item => res.status(200).json({
+                if(req.user._id.equals(item.recipient._id) || req.user._id.equals(item.sender._id)) {
+                    res.status(200).json({
                         success: true,
-                        message: "Message updated successfully.",
                         data: item
-                    }));
-            } else {
-                res.status(403).json({
-                    success: false,
-                    message: 'Access denied. User not permitted.'
-                });
-            }
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.deleteOne = (req, res) => {
-    return Message.findById(req.params.id)
-        .then(item => {
-            if(!item.length) return res.status(404).json({
+                    });
+                } else {
+                    res.status(403).json({
+                        success: false,
+                        message: 'Access denied. User not permitted.'
+                    });
+                }
+            })
+            .catch(err => res.status(500).json({
                 success: false,
-                message: `Message with ID ${req.params.id} not found.`
-            });
-            if(req.user._id.equals(item.sender._id)) {
-                item.remove({_id: req.params.id});
-                res.status(200).json({
-                    success: true,
-                    message: 'Match deleted successfully.'
-                });
-            } else {
-                res.status(403).json({
+                message: err
+            }));
+    },
+    getResourcesOfUser(req, res) {
+        return Message.find({$or:
+                [
+                    {'recipient': req.user._id},
+                    {'sender': req.user._id}
+                ]})
+            .then(item => {
+                if(!item.length) return res.status(404).json({
                     success: false,
-                    message: 'Access denied. User not permitted.'
+                    message: `Resources of user ID ${req.params.id} not found.`
                 });
-            }
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
+                if(req.user._id.equals(item.recipient._id) || req.user._id.equals(item.sender_id)) {
+                    res.status(200).json({
+                        success: true,
+                        data: item
+                    });
+                } else {
+                    res.status(403).json({
+                        success: false,
+                        message: 'Access denied. User not permitted.'
+                    });
+                }
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    getResourcesOfMatch(req, res) {
+        return Message.find({'match': req.params.id})
+            .then(item => {
+                if(!item.length) return res.status(404).json({
+                    success: false,
+                    message: `Resources of match ID ${req.params.id} not found.`
+                });
+                if(req.user._id.equals(item.recipient._id) || req.user._id.equals(item.sender._id)) {
+                    res.status(200).json({
+                        success: true,
+                        data: item
+                    });
+                } else {
+                    res.status(403).json({
+                        success: false,
+                        message: 'Access denied. User not permitted.'
+                    });
+                }
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    createNew(req, res) {
+        Match.findById(req.body.match)
+            .then(item => {
+                if(!item.length) {
+                    return res.status(404).json({
+                        success: false,
+                        message: `Match with ID ${req.body.match} not found.`
+                    });
+                }
+                if(!req.user._id.equals(item.user1._id) || !req.user._id.equals(item.user2._id)) {
+                    return res.status(403).json({
+                        success: false,
+                        message: 'Invalid match. User not permitted.'
+                    });
+                }
+            });
+        if(req.user._id.equals(req.body.recipient) || !req.user._id.equals(req.body.sender)) {
+            return res.status(403).json({
+                success: false,
+                message: 'Access denied. User not permitted.'
+            });
+        }
+        const message = new Message(req.body);
+        return message.save()
+            .then(item => res.status(201).json({
+                success: true,
+                message: "Message created successfully.",
+                data: item
+            }))
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    updateOne(req, res) {
+        return Message.findById(req.params.id)
+            .then(item => {
+                if(!item.length) return res.status(404).json({
+                    success: false,
+                    message: `Message with ID ${req.params.id} not found.`
+                });
+                if(req.user._id.equals(item.sender._id)) {
+                    const message = Object.assign(item, req.body);
+                    message.save()
+                        .then(item => res.status(200).json({
+                            success: true,
+                            message: "Message updated successfully.",
+                            data: item
+                        }));
+                } else {
+                    res.status(403).json({
+                        success: false,
+                        message: 'Access denied. User not permitted.'
+                    });
+                }
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    deleteOne(req, res) {
+        return Message.findById(req.params.id)
+            .then(item => {
+                if(!item.length) return res.status(404).json({
+                    success: false,
+                    message: `Message with ID ${req.params.id} not found.`
+                });
+                if(req.user._id.equals(item.sender._id)) {
+                    item.remove({_id: req.params.id});
+                    res.status(200).json({
+                        success: true,
+                        message: 'Match deleted successfully.'
+                    });
+                } else {
+                    res.status(403).json({
+                        success: false,
+                        message: 'Access denied. User not permitted.'
+                    });
+                }
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    }
+}
--- src/controllers/pendingMatchesController.js
+++ src/controllers/pendingMatchesController.js
@@ -1,168 +1,176 @@
 import PendingMatch from '../models/PendingMatch';
 import Match from '../models/Match';
 
-exports.getAll = (req, res) => {
-    const offset = parseInt(req.query.offset) || 0;
-    const perPage = parseInt(req.query.perPage) || 10;
-    PendingMatch.find({}).skip(offset).limit(perPage)
-        .populate({
-            path: 'user1',
-            select: 'name profilePicture'
-        })
-        .populate({
-            path: 'user2',
-            select: 'name profilePicture'
-        })
-        .then(item => {
-            if(!item) return res.status(404).json({ message: "Pending matches not found."});
-            return res.status(200).json({
-                success: true,
-                data: item
-            });
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.getOne = (req, res) => {
-    PendingMatch.findById(req.params.id)
-        .populate({
-            path: 'user1',
-            select: 'name profilePicture'
-        })
-        .populate({
-            path: 'user2',
-            select: 'name profilePicture'
-        })
-        .then(item => {
-            if(!item) return res.status(404).json({ message: `Pending match with ID ${req.params.id} not found.`});
-            if(req.user._id.equals(item.user1) || req.user._id.equals(item.user2)) {
-                return res.status(200).json({
+export default {
+    getAll(req, res) {
+        const offset = parseInt(req.query.offset) || 0;
+        const perPage = parseInt(req.query.perPage) || 10;
+        return PendingMatch.find({}).skip(offset).limit(perPage)
+            .populate({
+                path: 'user1',
+                select: 'name profilePicture'
+            })
+            .populate({
+                path: 'user2',
+                select: 'name profilePicture'
+            })
+            .then(item => {
+                if(!item.length) return res.status(404).json({
+                    success: false,
+                    message: "Pending matches not found."
+                });
+                res.status(200).json({
                     success: true,
                     data: item
                 });
-            }
-            else {
-                res.status(403).json({
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    getOne(req, res) {
+        return PendingMatch.findById(req.params.id)
+            .populate({
+                path: 'user1',
+                select: 'name profilePicture'
+            })
+            .populate({
+                path: 'user2',
+                select: 'name profilePicture'
+            })
+            .then(item => {
+                if(!item.length) return res.status(404).json({
                     success: false,
-                    message: 'Access denied. User not permitted.'
+                    message: `Pending match with ID ${req.params.id} not found.`
                 });
-            }
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.getResourcesOfUser = (req, res) => {
-    PendingMatch.find({$or:
-            [
-                {'user1': req.params.id},
-                {'user2': req.params.id}
-            ]})
-        .then(item => {
-            if(!item) return res.status(404).json({ message: `Resources of user ID ${req.params.id} not found.`});
-            if(req.user._id.equals(item.user1) || req.user._id.equals(item.user2)) {
-                return res.status(200).json({
-                    success: true,
-                    data: item
-                });
-            }
-            else {
-                res.status(403).json({
-                    success: false,
-                    message: 'Access denied. User not permitted.'
-                });
-            }
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.createNew = (req, res) => {
-    const pendingMatch = new PendingMatch(req.body);
-    pendingMatch.save()
-        .then(item => res.status(201).json({
-            success: true,
-            message: "Pending match created successfully.",
-            data: item
-        }))
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.updateOne = (req, res) => {
-    PendingMatch.findById(req.params.id)
-        .then(item => {
-            if(!item) return res.status(404).json({ message: `Pending match with ID ${req.params.id} not found.`});
-            if(req.user._id.equals(item.user1) || req.user._id.equals(item.user2)) {
-                if((req.user._id.equals(item.user1) && req.body.user2Approval) || (req.user._id.equals(item.user2) && req.body.user1Approval)) {
-                    return res.status(403).json({
+                if(req.user._id.equals(item.user1._id) || req.user._id.equals(item.user2._id)) {
+                    res.status(200).json({
+                        success: true,
+                        data: item
+                    });
+                } else {
+                    res.status(403).json({
                         success: false,
                         message: 'Access denied. User not permitted.'
                     });
                 }
-                const pendingMatch = Object.assign(item, req.body);
-                pendingMatch.save()
-                    .then(item => {
-                        if(item.user1Approval && item.user2Approval) {
-                            const newMatch = new Match(pendingMatch);
-                            newMatch.save();
-                            item.remove({_id: item._id});
-                            res.status(200).json({
-                                success: true,
-                                message: "New match created!",
-                                data: item
-                            });
-                        } else {
-                            res.status(200).json({
-                                success: true,
-                                message: "Pending match updated successfully.",
-                                data: item
-                            });
-                        }
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    getResourcesOfUser(req, res) {
+        return PendingMatch.find({$or:
+                [
+                    {'user1': req.params.id},
+                    {'user2': req.params.id}
+                ]})
+            .then(item => {
+                if(!item.length) return res.status(404).json({
+                    success: false,
+                    message: `Resources of user ID ${req.params.id} not found.`
+                });
+                if(req.user._id.equals(item.user1._id) || req.user._id.equals(item.user2._id)) {
+                    res.status(200).json({
+                        success: true,
+                        data: item
                     });
-            }
-            else {
-                res.status(403).json({
+                } else {
+                    res.status(403).json({
+                        success: false,
+                        message: 'Access denied. User not permitted.'
+                    });
+                }
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    createNew(req, res) {
+        const pendingMatch = new PendingMatch(req.body);
+        return pendingMatch.save()
+            .then(item => res.status(201).json({
+                success: true,
+                message: "Pending match created successfully.",
+                data: item
+            }))
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    updateOne(req, res) {
+        return PendingMatch.findById(req.params.id)
+            .then(item => {
+                if(!item.length) return res.status(404).json({
                     success: false,
-                    message: 'Access denied. User not permitted.'
+                    message: `Pending match with ID ${req.params.id} not found.`
                 });
-            }
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.deleteOne = (req, res) => {
-    PendingMatch.findById(req.params.id)
-        .then(item => {
-            if(!item) return res.status(404).json({ message: `Pending match with ID ${req.params.id} not found.`});
-            if(req.user._id.equals(item.user1) || req.user._id.equals(item.user2)) {
-                item.remove({_id: req.params.id});
-                res.status(200).json({
-                    success: true,
-                    message: 'Pending match deleted successfully.'
-                });
-            }
-            else {
-                res.status(403).json({
+                if(req.user._id.equals(item.user1._id) || req.user._id.equals(item.user2._id)) {
+                    if((req.user._id.equals(item.user1._id) && req.body.user2Approval) || (req.user._id.equals(item.user2._id) && req.body.user1Approval)) {
+                        return res.status(403).json({
+                            success: false,
+                            message: 'Access denied. User not permitted.'
+                        });
+                    }
+                    const pendingMatch = Object.assign(item, req.body);
+                    pendingMatch.save()
+                        .then(item => {
+                            if(item.user1Approval && item.user2Approval) {
+                                const newMatch = new Match(pendingMatch);
+                                newMatch.save();
+                                item.remove({_id: item._id});
+                                res.status(200).json({
+                                    success: true,
+                                    message: "New match created!",
+                                    data: item
+                                });
+                            } else {
+                                res.status(200).json({
+                                    success: true,
+                                    message: "Pending match updated successfully.",
+                                    data: item
+                                });
+                            }
+                        });
+                } else {
+                    res.status(403).json({
+                        success: false,
+                        message: 'Access denied. User not permitted.'
+                    });
+                }
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    deleteOne(req, res) {
+        return PendingMatch.findById(req.params.id)
+            .then(item => {
+                if(!item.length) return res.status(404).json({
                     success: false,
-                    message: 'Access denied. User not permitted.'
+                    message: `Pending match with ID ${req.params.id} not found.`
                 });
-            }
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
+                if(req.user._id.equals(item.user1._id) || req.user._id.equals(item.user2._idd)) {
+                    item.remove({_id: req.params.id});
+                    res.status(200).json({
+                        success: true,
+                        message: 'Pending match deleted successfully.'
+                    });
+                } else {
+                    res.status(403).json({
+                        success: false,
+                        message: 'Access denied. User not permitted.'
+                    });
+                }
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    }
+}
