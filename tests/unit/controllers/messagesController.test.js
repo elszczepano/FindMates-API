0bca39409d13a130fd84681d41b154d635ff1499
Dominik <szczepaniak.dominik98@gmail.com>
2019-01-24 18:11:17 +0100

    Remove useless tests.

--- tests/unit/controllers/messagesController.test.js
+++ tests/unit/controllers/messagesController.test.js
@@ -1,26 +1,2 @@
 import messagesController from '../../../src/controllers/messagesController';
 import assert from 'assert';
-
-describe('Test if messages methods exists', () => {
-    it('getAll method should be defined', () => {
-        assert(typeof messagesController.getAll === 'function');
-    });
-    it('getOne method should be defined', () => {
-        assert(typeof messagesController.getOne === 'function');
-    });
-    it('getResourcesOfUser method should be defined', () => {
-        assert(typeof messagesController.getResourcesOfUser === 'function');
-    });
-    it('getResourcesOfMatch method should be defined', () => {
-        assert(typeof messagesController.getResourcesOfMatch === 'function');
-    });
-    it('createNew method should be defined', () => {
-        assert(typeof messagesController.createNew === 'function');
-    });
-    it('updateOne method should be defined', () => {
-        assert(typeof messagesController.updateOne === 'function');
-    });
-    it('deleteOne method should be defined', () => {
-        assert(typeof messagesController.deleteOne === 'function');
-    });
-});
