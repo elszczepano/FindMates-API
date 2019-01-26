4197ec232c89f157d494378d5a397919c433c44d
Dominik <szczepaniak.dominik98@gmail.com>
2019-01-26 16:35:18 +0100

    Fix bugs. | Prettifuy the code.

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,133 +1,150 @@
 import User from '../models/User';
 import moment from 'moment';
 
-exports.getAll = (req, res) => {
-    const offset = parseInt(req.query.offset) || 0;
-    const perPage = parseInt(req.query.perPage) || 3;
-    User.find({}).skip(offset).limit(perPage)
-        .then(item => {
-            if(!item) return res.status(404).json({ message: "Users not found."});
-            return res.status(200).json({
-                success: true,
-                data: item
-            });
+export default {
+    getAll(req, res) {
+        const offset = parseInt(req.query.offset) || 0;
+        const perPage = parseInt(req.query.perPage) || 3;
+        return User.find({}).skip(offset).limit(perPage)
+            .then(item => {
+                if(!item.length || !item) return res.status(404).json({
+                    success: false,
+                    message: "Users not found."
+                });
+                res.status(200).json({
+                    success: true,
+                    data: item
+                });
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    getOne(req, res) {
+        return User.findById(req.params.id)
+            .then(item => {
+                if(!item) return res.status(404).json({
+                    success: false,
+                    message: `User with ID ${req.params.id} not found.`
+                });
+                res.status(200).json({
+                    success: true,
+                    data: item
+                });
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    findNearby(req, res) {
+        const offset = parseInt(req.query.offset) || 0;
+        const perPage = parseInt(req.query.perPage) || 3;
+        return User.aggregate().near({
+            near: [parseFloat(req.query.lng), parseFloat(req.query.lat)],
+            maxDistance: req.query.distance,
+            spherical: true,
+            distanceField: 'dist.calculated'
         })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.getOne = (req, res) => {
-    User.findById(req.params.id)
-        .then(item => {
-            if(!item) return res.status(404).json({ message: `User with ID ${req.params.id} not found.`});
-            return res.status(200).json({
-                success: true,
-                data: item
-            });
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.findNearby = (req, res) => {
-    const offset = parseInt(req.query.offset) || 0;
-    const perPage = parseInt(req.query.perPage) || 3;
-    User.aggregate().near({
-        near: [parseFloat(req.query.lng), parseFloat(req.query.lat)],
-        maxDistance: req.query.distance,
-        spherical: true,
-        distanceField: 'dist.calculated'
-        })
-        .match({
-            gender: req.params.gender,
-            $and: [
-                { birthDate: { $gte: moment().subtract(req.params.minAge, 'years') } },
-                { birthDate: { $lte: moment().subtract(req.params.maxAge, 'years') } }
-            ]
-        }).skip(offset).limit(perPage)
-        .then(item => {
-            if(!item) return res.status(404).json({ message: `There are no people nearby`});
-            return res.status(200).json({
-                success: true,
-                data: item
-            });
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.updateOne = (req, res) => {
-    User.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        .then(item => {
-            if(!item) return res.status(404).json({ message: `User with ID ${req.params.id} not found.`});
-            return res.status(200).json({
-                success: true,
-                data: item
-            });
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.updateProfilePicture = (req, res) => {
-    User.findById(req.params.id)
-        .then(item => {
-            if(!item) return res.status(404).json({ message: `User with ID ${req.params.id} not found.`});
+            .match({
+                gender: req.params.gender,
+                $and: [
+                    { birthDate: { $gte: moment().subtract(req.params.minAge, 'years') } },
+                    { birthDate: { $lte: moment().subtract(req.params.maxAge, 'years') } }
+                ]
+            }).skip(offset).limit(perPage)
+            .then(item => {
+                if(!item.length || !item) return res.status(200).json({
+                    success: true,
+                    message: `There are no people nearby`
+                });
+                res.status(200).json({
+                    success: true,
+                    data: item
+                });
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    updateOne(req, res) {
+        return User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+            .then(item => {
+                if(!item) return res.status(404).json({
+                    success: false,
+                    message: `User with ID ${req.params.id} not found.`
+                });
+                res.status(200).json({
+                    success: true,
+                    data: item
+                });
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    updateProfilePicture(req, res) {
+        User.findById(req.params.id)
+            .then(item => {
+                if(!item) return res.status(404).json({
+                    success: false,
+                    message: `User with ID ${req.params.id} not found.`
+                });
                 const user = Object.assign(item, {profilePicture: req.file.path});
-                user.save()
+                return user.save()
                     .then(item => res.status(200).json({
                         success: true,
                         message: "User profile picture updated successfully.",
                         data: item
                     }));
 
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.updateGallery = (req, res) => {
-    const paths = req.files.map(file => file.path);
-    User.findById(req.params.id)
-        .then(item => {
-            if(!item) return res.status(404).json({ message: `User with ID ${req.params.id} not found.`});
-            const user = Object.assign(item, {pictures: paths});
-            user.save()
-                .then(item => res.status(200).json({
-                    success: true,
-                    message: "User gallery updated successfully.",
-                    data: item
-                }));
-
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
-
-exports.deleteOne = (req, res) => {
-    User.findById(req.params.id)
-        .then(item => {
-            if(!item) return res.status(404).json({ message: `User with ID ${req.params.id} not found.`});
-            item.remove();
-            res.json({
-                success: true,
-                message: 'User deleted successfully.'
             })
-        })
-        .catch(err => res.status(500).json({
-            success: false,
-            message: err
-        }));
-};
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    updateGallery(req, res) {
+        const paths = req.files.map(file => file.path);
+        User.findById(req.params.id)
+            .then(item => {
+                if(!item) return res.status(404).json({
+                    success: false,
+                    message: `User with ID ${req.params.id} not found.`
+                });
+                const user = Object.assign(item, {pictures: paths});
+                return user.save()
+                    .then(item => res.status(200).json({
+                        success: true,
+                        message: "User gallery updated successfully.",
+                        data: item
+                    }));
+
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    },
+    deleteOne(req, res) {
+        return User.findById(req.params.id)
+            .then(item => {
+                if(!item) return res.status(404).json({
+                    success: false,
+                    message: `User with ID ${req.params.id} not found.`
+                });
+                item.remove();
+                res.status(200).json({
+                    success: true,
+                    message: 'User deleted successfully.'
+                })
+            })
+            .catch(err => res.status(500).json({
+                success: false,
+                message: err
+            }));
+    }
+}
